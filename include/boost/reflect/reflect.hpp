ad37a756e7d7e404e47ab490861ece0808ee836b
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2014-09-17 11:24:36 +0700

    Fix visitor call error

--- include/boost/reflect/reflect.hpp
+++ include/boost/reflect/reflect.hpp
@@ -56,7 +56,7 @@ struct reflector{
   boost::reflect::reflector<base>::visit( visitor );
 
 #define BOOST_REFLECT_VISIT_MEMBER( r, visitor, elem ) \
-  visitor.template operator()<BOOST_TYPEOF(&type::elem), &type::elem>( BOOST_PP_STRINGIZE(elem) );
+  visitor.template operator()<BOOST_TYPEOF(type::elem), &type::elem>( BOOST_PP_STRINGIZE(elem) );
 
 #define BOOST_REFLECT_DERIVED_IMPL_INLINE( TYPE, INHERITS, MEMBERS ) \
 template<typename Visitor>\
